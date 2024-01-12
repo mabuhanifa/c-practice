c1a811acb85ef552779e1edcfe3b0d1104a5f693
mabuhanifa <moh.abuhanifa@gmail.com>
2024-01-12 22:25:09 +0600

    C Data Types

--- code.c
+++ code.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+// Create variables
 
 int main()
 {
+    int myNum = 5;           // Integer (whole number)
+    float myFloatNum = 5.99; // Floating point number
+    char myLetter = 'D';     // Character
+
+    // Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+
     int age = 25;
     float pi = 3.14;
     char grade = 'A';
