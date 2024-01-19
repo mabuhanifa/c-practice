21114dc65f2e1409b0523eb15de29d1b7baad2c4
mabuhanifa <moh.abuhanifa@gmail.com>
2024-01-19 23:54:33 +0600

    Operators

--- code.c
+++ code.c
@@ -18,6 +18,18 @@ int main()
 
     int x = 5, y = 6, z = 50;
     printf("%d", x + y + z);
+    /*
 
+    Operator 	        Name 	        Description 	                                                  Example
+        + 	        Addition 	        Adds together two values 	                                        x + y
+        - 	        Subtraction 	    Subtracts one value from another 	                                x - y
+        * 	        Multiplication 	    Multiplies two values 	                                            x * y
+        / 	        Division 	        Divides one value by another 	                                    x / y
+        % 	        Modulus 	        Returns the division remainder 	                                    x % y
+        ++ 	        Increment 	        Increases the value of a variable by 1 	                            ++x
+        -- 	        Decrement 	           Decreases the value of a variable by 1 	-                       -x
+
+
+    */
     return 0;
 }
